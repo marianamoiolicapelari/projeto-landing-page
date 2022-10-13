e088d4f99320f549da3f472267a8268a5f5e17e5
Mariana M.C <marianamoioli@gmail.com>
2022-10-13 18:20:21 -0300

    added new features

--- js/script.js
+++ js/script.js
@@ -1,23 +1,17 @@
 const btnOpenModal = document.getElementById('btn-navbar')
 
 
-const openModal = () => { document.getElementById('modal')
-    .classList.add('active')
+
+const openModal = () => {
+    document.getElementById('modal')
+    .classList.add('.active')
 
 
-    console.log(modal)
+    console.log('retornou')
 }
 
-
-
-
-    const closeModal = () => {
-    //clearFields()
-    document.getElementById('modal').classList.remove('active')
-}
 
 
 
 btnOpenModal.addEventListener('click', openModal)
 
-document.addEventListener('click', closeModal)
